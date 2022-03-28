6b806904b1866dda7eb40f6925c5c1ce6346a2ce
ibrahim cherifi <69094349+brahimch217@users.noreply.github.com>
2022-03-28 01:03:38 -0700

    update

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require('express');
 const port = process.env.PORT || 8000
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 /**
  * Import Model
